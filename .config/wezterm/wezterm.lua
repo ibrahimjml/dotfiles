89a345d8619c442b9aeb7bb37c5f63a769cc5a75
ibrahimjml <169608250+ibrahimjml@users.noreply.github.com>
2026-09-22 17:56:23 +0300

    added Cmder + starship

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,13 +2,12 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local custom_keys = require("commands.keys")
 local display = require("display")
+local shell_init = require("shell")
 
 -- Configure appearance settings
-
 display.setup(config)
 
 -- Toggle tab bar visibility
-
 wezterm.on("toggle-tabbar", function(window)
 	local overrides = window:get_config_overrides() or {}
 	if overrides.enable_tab_bar == false then
@@ -20,7 +19,9 @@ wezterm.on("toggle-tabbar", function(window)
 end)
 
 -- Configure custom keybindings
-
 config.keys = custom_keys.setup_keys()
+
+-- shell init 
+shell_init.apply_shell_config(config)
 
 return config
